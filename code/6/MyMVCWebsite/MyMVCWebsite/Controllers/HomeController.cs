5a78b628d8eaae4052a63f33181e178f648f2446
Kan1shka9 <kanishka.s9@gmail.com>
2017-10-18 11:50:08 -0700

    6_Diagnostics-and-Debugging.md

---
+++ code/6/MyMVCWebsite/MyMVCWebsite/Controllers/HomeController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MyMVCWebsite.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            Trace.TraceInformation("Index page request");
+            Trace.WriteLine("Just a general verbose trace message");
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+            Trace.TraceInformation("About page request");
+            Trace.TraceWarning("Warning on the about page");
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+            Trace.TraceInformation("Contact page request");
+            Trace.TraceError("Error on the contact page");
+            return View();
+        }
+    }
+}
