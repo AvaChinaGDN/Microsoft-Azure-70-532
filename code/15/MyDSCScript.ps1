ba67a6b4fcb9e5e9f7893635f4bd22340464814d
Kan1shka9 <kanishka.s9@gmail.com>
2017-10-19 01:43:23 -0700

    15_Custom-Extensions.md

---
+++ code/15/MyDSCScript.ps1
@@ -0,0 +1,20 @@
+﻿configuration DeployWebPage
+{
+	node("localhost")
+	{
+		WindowsFeature IIS
+		{
+			Ensure = "Present"
+			Name = "Web-Server"
+		}
+
+		File WebPage
+		{
+			Ensure = "Present"
+			DestinationPath = "C:\inetpub\wwwroot\index.html"
+			Force = $true
+			Type = "File"
+			Contents = "<html><body><h1>Hello from DSC!</h1></body></html>"
+		}
+	}
+}
